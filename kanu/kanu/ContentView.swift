05d4a637484ef7a30a0d50bb1926434853cc3bf8
Soongyu Kwon <me@soongyu.me>
2024-03-23 14:44:52 +0900

    swiftui proj init

---
+++ kanu/kanu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kanu
+//
+//  Created by Soongyu Kwon on 3/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kanu/kanu/kanuApp.swift
@@ -0,0 +1,17 @@
+//
+//  kanuApp.swift
+//  kanu
+//
+//  Created by Soongyu Kwon on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct kanuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
