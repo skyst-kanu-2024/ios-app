612424f3831eeba9c5ddb8c5f4fff2fde4a2e217
Soongyu Kwon <me@soongyu.me>
2024-03-23 23:36:09 +0900

    23

--- kanu/Views/NearbyView.swift
+++ kanu/Views/NearbyView.swift
@@ -14,10 +14,6 @@ import Alamofire
 struct NearbyView: View {
     @ObservedObject var viewModel = NearbyViewModel()
     
-//    init(viewModel: NearbyViewModel) {
-//        self.viewModel = viewModel
-//    }
-    
     var body: some View {
         ZStack {
             Color.black
@@ -28,7 +24,7 @@ struct NearbyView: View {
                         Text("대충엄청난닉네임")
                             .foregroundStyle(.gray)
                             .font(.caption)
-                        Text("\(String(format: "%.2f", 0.0)) m")
+                        Text("\(String(format: "%.2f", self.viewModel.distanceToPeerDevice)) m")
                             .foregroundStyle(.white)
                             .fontWeight(.bold)
                             .font(.largeTitle)
@@ -46,24 +42,52 @@ struct NearbyView: View {
     
     func startup() {
         viewModel.startNearbySession()
-        
-        let parameter: [String: String] = [
-            "roomid": "1",
-            "userid": "1",
-            "token": "hello_there"
-        ]
-        let tokenUpload = AF.request("https://wget.kr/dtoken", method: .post, parameters: parameter, encoder: JSONParameterEncoder.default)
-        
-        tokenUpload.response() { response in
-            print(response.response?.statusCode)
+        if let discoveryToken = viewModel.getLocalDiscoveryToken() {
+            if let encodedToken = try? NSKeyedArchiver.archivedData(withRootObject: discoveryToken, requiringSecureCoding: true) {
+                print(encodedToken.base64EncodedString())
+                
+                let parameter: [String: String] = [
+                    "roomid": "3",
+                    "userid": "1",
+                    "token": encodedToken.base64EncodedString()
+                ]
+                let tokenUpload = AF.request("https://wget.kr/dtoken", method: .post, parameters: parameter, encoder: JSONParameterEncoder.default)
+                
+                tokenUpload.response() { response in
+                    print(response.response?.statusCode)
+                }
+            }
         }
-        
-//        let tokenGet = AF.request("https://wget.kr/dtoken?roomid=1&userid=2", method: .get)
-//
-//        tokenGet.response() { response in
-//            print(response.response?.statusCode)
-//        }
+        getPeerToken()
     }
+    
+    func getPeerToken() {
+        AF.request("https://wget.kr/dtoken?roomid=3&userid=1", method: .get).responseData() { response in
+            switch response.result {
+            case .success(let data):
+                do {
+                    let decoder = JSONDecoder()
+                    decoder.dateDecodingStrategy = .iso8601
+                    let json = try decoder.decode(Peer.self, from: data)
+                    if let data = Data(base64Encoded: json.token) {
+                        if let decodedToken = try? NSKeyedUnarchiver.unarchivedObject(ofClass: NIDiscoveryToken.self, from: data) {
+                            print(decodedToken)
+                        }
+                    }
+                } catch {
+                    print("error")
+                    getPeerToken()
+                }
+            case .failure(let error):
+                print(error)
+                getPeerToken()
+            }
+        }
+    }
+}
+
+struct Peer: Codable, Hashable {
+    let token: String
 }
 
 #Preview {
